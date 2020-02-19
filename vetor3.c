9bf8d1d24fae6d8939f8c73af6f712700c718aad
Aluno <lucasflorentino_2000@hotmail.com>
2020-02-18 22:28:50 -0300

    Exercicio Vetor 3 resolvido

--- vetor3.c
+++ vetor3.c
@@ -4,14 +4,30 @@
 int main ()
 {
 
-	int *p;
+	int qtdeAlunos, i, *p, *idades;
 
-	p = malloc(sizeof(int));
+	idades = malloc(sizeof(int));
 
 	if (p == NULL) {
 		printf("Deu pau ! ");
 		exit(1);
 	}
 
+	p = idades;
+	for(i=0; i<qtdeAlunos; i++){
+		printf("idade %i: ", i);
+		scanf("%i", p);
+		p++;
+	}
+	
+	
+	p = idades;
+	for(i=0; i<qtdeAlunos; i++)
+		printf("idade %i: %i\n", i, *p);
+		p++;
+	
 	return 0;
+
+
+	
 }
